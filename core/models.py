a696e41cffe58fe64d089e1ebbfbdf72ddf99c3a
itsregalo <itsregalo047@gmail.com>
2023-04-14 01:39:34 +0300

    removed .csv large

--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Crime(models.Model):
+    id = models.IntegerField(primary_key=True)
+    case_number = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    block = models.CharField(max_length=50)
+    iucr = models.CharField(max_length=10)
+    primary_type = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    location_description = models.CharField(max_length=100)
+    arrest = models.BooleanField()
+    domestic = models.BooleanField()
+    beat = models.IntegerField()
+    district = models.IntegerField()
+    ward = models.FloatField(null=True, blank=True)
+    community_area = models.IntegerField()
+    fbi_code = models.CharField(max_length=10)
+    x_coordinate = models.FloatField(null=True, blank=True)
+    y_coordinate = models.FloatField(null=True, blank=True)
+    year = models.IntegerField()
+    updated_on = models.DateTimeField()
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.case_number
+    
+    class Meta:
+        managed = False
+        db_table = 'crime'
---
+++ to_do.md
@@ -0,0 +1,7 @@
+Crime data input: Provide a form or file upload functionality that allows users to input their own crime data for prediction.
+Prediction output: Display the predicted probability or binary prediction (arrest or no arrest) based on the input crime data.
+Visualization: Provide visualizations that display the crime patterns in Indore, such as a heatmap or a bar chart showing the frequency of different types of crimes.
+Model selection: Allow users to select which classification algorithm they want to use for prediction (e.g., Gaussian Naive Bayes, Decision Tree, Random Forest, or Logistic Regression).
+Model performance evaluation: Display the performance metrics (confusion matrix, accuracy, error, precision, recall, and F1 score) for the selected model.
+Explanation of results: Provide an explanation of how the model arrived at its prediction, such as which features were most important in the prediction or which crime categories were most closely associated with an arrest.
+Future work: Provide suggestions for future work based on the results of the model, such as which crime categories or regions may need more attention from law enforcement or which features could be added to improve the model's performance.
