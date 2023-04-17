0d4a69e5b1d1ca0a395fde92ce238b63ab659eb8
itsregalo <itsregalo047@gmail.com>
2023-04-17 20:49:35 +0300

    import successfull

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,9 @@
 
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import Crime
 
-# Register your models here.
-class CrimeAdmin(admin.ModelAdmin):
+class CrimeAdmin(ImportExportModelAdmin):
     list_display = ('case_number', 'date', 'block', 'primary_type', 'description', 'location_description', 'arrest', 'domestic', 'beat', 'district',
                     'ward', 'community_area', 'fbi_code', 'x_coordinate', 'y_coordinate', 'year', 'updated_on', 'latitude', 'longitude', 'location')
     list_filter = ('date', 'primary_type', 'description', 'location_description', 'arrest', 'domestic', 'beat',
@@ -12,6 +12,5 @@ class CrimeAdmin(admin.ModelAdmin):
                      'ward', 'community_area', 'fbi_code', 'x_coordinate', 'y_coordinate', 'year', 'updated_on', 'latitude', 'longitude', 'location')
     ordering = ('date',)
     list_per_page = 25
-
 
 admin.site.register(Crime, CrimeAdmin)
